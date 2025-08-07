43c065854014b7744e024b10ab704d06a25478ef
Kevin Muñoz <125827081+KevinJJ01@users.noreply.github.com>
2025-08-06 20:03:16 -0500

    Implemeentation of asistance confirm

--- invitation/views.py
+++ invitation/views.py
@@ -1,6 +1,8 @@
+import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from invitation.logic.invitation_logic import ingresarFirebase, confirmacionfunction
+from django.http import JsonResponse
 
 @csrf_exempt  # Solo para pruebas locales
 def ingreso(request):
@@ -16,11 +18,11 @@ def ingreso(request):
 
 @csrf_exempt  # Solo para pruebas locales
 def confirmacion(request):
-    if request.method == 'PATCH':
-        datos = request.PATCH.get()
-        if not datos:
-            return JsonResponse({'error': 'Datos no encontrados'}, status=400)
-        
-        return confirmacionfunction(datos)
-    
-    return JsonResponse({'error': 'Método no permitido'}, status=405)
+    if request.method == 'POST':
+        try:
+            datos = json.loads(request.body)  # Accede al JSON enviado
+            print(datos)  # {'invitado1': true, 'invitado2': false, ...}
+            # Procesamiento aquí...
+            return confirmacionfunction(datos)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "JSON inválido"}, status=400)
